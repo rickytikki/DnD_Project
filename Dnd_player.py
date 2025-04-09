9ddddd020e2171fb4d100a914412484d0b12b97b
rickytikki <rthompson1217@gmail.com>
2025-04-09 06:06:25 -0400

    updated player class to add level of player and level up set function

--- Dnd_player.py
+++ Dnd_player.py
@@ -1,9 +1,13 @@
 class Player:
 
-    def __init__(self,name,bonus):
+    def __init__(self,name,bonus,lvl):
         self.name = name
         self.bonus = bonus
+        self.level = lvl
     
     def set_initiative(self, roll):
         self.initiative = roll + self.bonus
+    
+    def set_Lvl_Up(self, lvl):
+        self.level = lvl
         
