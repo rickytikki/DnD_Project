80c66c01011b5c22c14d9af302ae2905f300f9ca
rickytikki <rthompson1217@gmail.com>
2025-04-12 20:35:50 -0400

    added 2 new functions to the player class for setting a condition to a player and printing current info on the player. Also started adding comments

--- Dnd_player.py
+++ Dnd_player.py
@@ -1,13 +1,23 @@
 class Player:
+#This Class is for the playable Character in a DnD Campaign#
 
+#the first function initializes the Player with their name, inititive bonus, and level (for now)#
     def __init__(self,name,bonus,lvl):
         self.name = name
         self.bonus = bonus
         self.level = lvl
+        self.condition = ""
     
+    #sets the players initiative to what they rolled + the bonus they had to start#
     def set_initiative(self, roll):
         self.initiative = roll + self.bonus
     
-    def set_Lvl_Up(self, lvl):
+    def Lvl_Up(self, lvl):
         self.level = lvl
         
+    def set_condition(self,status):
+        self.condition = status
+    
+    def Check_Player(self):
+        print(self.name)
+    
